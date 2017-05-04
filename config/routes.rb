70353042ad9e97f9824d1563997570f731a07d60
Megan Wiley <mwiley322@gmail.com>
2017-05-04 11:55:10 -0700

    Add devise controllers and configure root to dashboard upon signup and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,29 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: "home#index"
+  devise_scope :user do
+    get "signup", to: "devise/registrations#new"
+    get "login", to: "devise/sessions#new"
+    get "logout", to: "devise/sessions#destroy"
+
+    authenticated :user do
+      root 'goals#index', as: :authenticated_root
+  end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+end
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
+  get '/dashboard' => "goals#index"
+
+  get '/applications' => "applications#index"
+
+  get '/interviews' => "interviews#index"
+
+  get '/account_settings' => "users#show"
+
 end
