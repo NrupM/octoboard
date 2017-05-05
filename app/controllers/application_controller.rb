5242dfcfb211f14ec7e79687a7ef7147f146cf85
Megan Wiley <mwiley322@gmail.com>
2017-05-05 16:43:50 -0700

    Edit helper method and conditional views for interview page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :is_value_nil? 
+  helper_method :is_value_nil?
 
   def is_value_nil?(attribute_value)
-    unless attribute_value.size < 1
+    unless attribute_value.to_s.size < 1 
       return true
     end
   end
