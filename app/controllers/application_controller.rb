b1adbd84b57cf216f641e5ca71805aa508c64027
Megan Wiley <mwiley322@gmail.com>
2017-05-05 16:27:34 -0700

    Add helper method in applications controller to check for nil form values

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :is_value_nil? 
+
+  def is_value_nil?(attribute_value)
+    unless attribute_value.size < 1
+      return true
+    end
+  end
+
 end
