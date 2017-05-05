a589fe465b7bbdf826e9a60fc37cdbac437cd37d
Megan Wiley <mwiley322@gmail.com>
2017-05-05 11:46:37 -0700

    Add create function for new interview

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -7,6 +7,13 @@ class InterviewsController < ApplicationController
     @interview = Interview.new
   end
   def create
+    @interview = current_user.interviews.create(interview_params)
+    if @interview.save
+      redirect_to interviews_path
+    else
+      flash[:error] = 'An error occurred saving your interview details. Please try again.'
+      redirect_to :back
+    end
   end
   def show
   end
@@ -15,6 +22,12 @@ class InterviewsController < ApplicationController
   def update
   end
   def destroy
+  end
+
+  private
+
+  def interview_params
+    params.require(:interview).permit(:interview_date, :interview_type, :is_followup_needed, :primary_contact_name, :primary_contact_title, :primary_contact_email, :primary_contact_phone, :questions_to_ask, :thankyou_letter, :coding_challenge_url, :coding_challenge_due_date, :coding_challenge_prompt, :is_challenge_completed, :notes)
   end
 
 end
--- app/models/interview.rb
+++ app/models/interview.rb
@@ -4,6 +4,8 @@ class Interview < ApplicationRecord
 
   validates :is_followup_needed,
             :presence => { :if => 'is_followup_needed.nil?' }
+  validates :is_challenge_completed,
+            :presence => { :if => 'is_challenge_completed.nil?' }
 
   enum interview_types: [:phone, :online, :in_person]
 
