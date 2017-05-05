38cd8c360a9c49a0ef373c4022a4bdf0a1424746
Megan Wiley <mwiley322@gmail.com>
2017-05-05 12:41:20 -0700

    Edit interview show page and create controller function to make new interview for a specific job application

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -5,7 +5,9 @@ class InterviewsController < ApplicationController
   end
   def new
     @interview = Interview.new
+    @application = JobApplication.find_by_id(params[:job_application_id])
   end
+
   def create
     @interview = current_user.interviews.create(interview_params)
     if @interview.save
@@ -27,7 +29,7 @@ class InterviewsController < ApplicationController
   private
 
   def interview_params
-    params.require(:interview).permit(:interview_date, :interview_type, :is_followup_needed, :primary_contact_name, :primary_contact_title, :primary_contact_email, :primary_contact_phone, :questions_to_ask, :thankyou_letter, :coding_challenge_url, :coding_challenge_due_date, :coding_challenge_prompt, :is_challenge_completed, :notes)
+    params.require(:interview).permit(:job_application_id, :interview_date, :interview_type, :is_followup_needed, :primary_contact_name, :primary_contact_title, :primary_contact_email, :primary_contact_phone, :questions_to_ask, :thankyou_letter, :coding_challenge_url, :coding_challenge_due_date, :coding_challenge_prompt, :is_challenge_completed, :notes)
   end
 
 end
--- app/models/interview.rb
+++ app/models/interview.rb
@@ -2,10 +2,10 @@ class Interview < ApplicationRecord
   belongs_to :job_application
   belongs_to :user
 
-  validates :is_followup_needed,
-            :presence => { :if => 'is_followup_needed.nil?' }
-  validates :is_challenge_completed,
-            :presence => { :if => 'is_challenge_completed.nil?' }
+  # validates :is_followup_needed,
+  #           :presence => { :if => 'is_followup_needed.nil?' }
+  # validates :is_challenge_completed,
+  #           :presence => { :if => 'is_challenge_completed.nil?' }
 
   enum interview_types: [:phone, :online, :in_person]
 
