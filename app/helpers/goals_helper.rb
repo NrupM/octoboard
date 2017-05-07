891d29a63007ef366caad25bd172f25465af016c
Megan Wiley <mwiley322@gmail.com>
2017-05-07 10:03:13 -0700

    Add greet function in goal helper

--- app/helpers/goals_helper.rb
+++ app/helpers/goals_helper.rb
@@ -9,4 +9,23 @@ module GoalsHelper
     current_time < date
   end
 
+  def greet
+    now = Time.now
+    today = Date.today.to_time
+
+    morning = today.beginning_of_day
+    noon = today.noon
+    evening = today.change( hour: 17 )
+    night = today.change( hour: 20 )
+    tomorrow = today.tomorrow
+
+    if (morning..noon).cover? now
+      'GOOD MORNING'
+    elsif (noon..evening).cover? now
+      'GOOD AFTERNOON'
+    else (evening..tomorrow).cover? now
+      'GOOD EVENING'
+    end
+  end
+
 end
