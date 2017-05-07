3f81d9086784afd4e6c2e0e766c478fbaba406aa
Megan Wiley <mwiley322@gmail.com>
2017-05-07 00:06:11 -0700

    Layout on main page with divs for web view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,22 +15,15 @@
 //= require_tree .
 
 $( document ).on('turbolinks:load', function() {
-  console.log("It works on each visit!")
-  // // Initialize collapse button
   $(".button-collapse").sideNav();
-  // // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-  // $('.collapsible').collapsible();
-
   $('select').material_select();
   $('#textarea1').val('New Text');
   $('#textarea1').trigger('autoresize');
-
   $('.datepicker').pickadate({
-   selectMonths: true, // Creates a dropdown to control month
-   selectYears: 15 // Creates a dropdown of 15 years to control year
- });
-
-})
+    selectMonths: true,
+    selectYears: 15
+  });
+});
 
 function showSweetAlertConfirmModal(e) {
   swal({
