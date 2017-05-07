98a4b23dfefacad1b2220cdc044d393114918fb7
Megan Wiley <mwiley322@gmail.com>
2017-05-07 00:50:12 -0700

    Add mobile friendly response for main user div on main dash

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,13 @@ class User < ApplicationRecord
   has_many :goals, dependent: :destroy
   has_many :job_applications, dependent: :destroy
   has_many :interviews, dependent: :destroy
+  after_initialize :set_defaults, unless: :persisted?
+
+
+  def set_defaults
+    self.octopower  ||= 10
+    self.image  ||= 'https://d28rk61hailme.cloudfront.net/assets/default_user-856f2487c07862f3089cfcb1528df354.png'
+  end
 
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
