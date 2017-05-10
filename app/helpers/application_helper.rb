d9f60a0f8a831f1cc1541a324a6b680b7be13e47
Megan Wiley <mwiley322@gmail.com>
2017-05-09 17:35:34 -0700

    Repaired check for http function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,12 @@
 module ApplicationHelper
 
   def check_for_http url
-    if url[0...6] != 'http://' && url[0..6] != 'https://'
-      return 'http://' + url
+    if url[0..7] == 'https://' || url[0..6] == 'http://'
+      return url
+    elsif url[0..3] == 'www.'
+      return 'https://'+url
+    else
+      return 'https://'+url
     end
   end
 
